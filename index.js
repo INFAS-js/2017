e4ae69bd50b88bb19459c9c63d86e0eaed2e21fd
DeemoHarlos <cces4415@gmail.com>
2017-05-29 00:56:35 +0800

    V 3.4.5 mainpage roughly complete on PC

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+var maploaded=0;
 function page(a){
 	clear();
 	document.getElementById(a).style.display = "block";
-	if(a == "trans"){loadmap();}
+	if(a == "trans" && !maploaded){loadmap();maploaded = 1;}
 }
 function clear(){
 	document.getElementById("intro").style.display = "none";
@@ -11,9 +12,13 @@ function clear(){
 	document.getElementById("sponsor").style.display = "none";
 	document.getElementById("history").style.display = "none";
 }
+
 function loadmap(){
 	var map = L.map('map').setView([24.7775,121.0175], 16);
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
+	L.marker([24.77676,121.01711]).addTo(map)
+	    .bindPopup('新竹市東區介壽路300號')
+	    .openPopup();
 }
